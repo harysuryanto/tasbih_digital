c0397a1c6ae6b88a3f0e071fb254fa4272d514d4
harysuryanto <hary.suryanto01@gmail.com>
2021-10-03 23:22:45 +0700

    change swipe mechanism; extract dialog, list tile, and remove item to a separated method

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swipe_action_cell/core/cell.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import 'package:tasbih_digital/controllers/tasbih_controller.dart';
-import 'package:tasbih_digital/models/tasbih_model.dart';
 
 class HomeScreen extends StatelessWidget {
   final tasbihController = Get.put(TasbihController());
+  final textEditingController = TextEditingController();
+  final textFieldFocusNode = FocusNode();
 
   @override
   Widget build(BuildContext context) {
-    var textEditingController = TextEditingController();
-
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
       body: SafeArea(
@@ -30,36 +30,11 @@ class HomeScreen extends StatelessWidget {
                       Icons.add,
                       color: Colors.white,
                     ),
+                    padding: EdgeInsets.zero,
                     tooltip: 'Buat tasbih baru',
                     onPressed: () {
-                      showDialog(
-                        context: context,
-                        builder: (context) {
-                          return AlertDialog(
-                            title: Text('Tambah tasbih'),
-                            content: TextField(
-                              controller: textEditingController,
-                              autofocus: true,
-                              decoration: InputDecoration(
-                                  labelText: 'Tulis nama tasbih...'),
-                            ),
-                            actions: <Widget>[
-                              TextButton(
-                                child: const Text(
-                                  'Simpan',
-                                ),
-                                onPressed: () {
-                                  tasbihController.tasbihs.add(
-                                      Tasbih(name: textEditingController.text));
-                                  textEditingController.clear();
-                                  Get.back();
-                                },
-                              ),
-                            ],
-                            elevation: 24,
-                          );
-                        },
-                      );
+                      /// Show dialog
+                      _showTextEditingDialog(context: context);
                     },
                   ),
                 ],
@@ -77,90 +52,44 @@ class HomeScreen extends StatelessWidget {
                   itemCount: tasbihController.tasbihs.length,
                   separatorBuilder: (context, index) => SizedBox(height: 15),
                   itemBuilder: (context, index) {
-                    /// Formatting the date
-                    DateTime originalUpdatedAt = DateTime.parse(
-                        tasbihController.tasbihs[index].updatedAt);
-                    final DateFormat formatter = DateFormat('dd-MM-yyyy');
-                    final String formattedUpdatedAt =
-                        formatter.format(originalUpdatedAt);
-
-                    return Dismissible(
+                    return SwipeActionCell(
                       key: UniqueKey(),
-                      background: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [Icon(Icons.delete_outline_rounded)],
-                      ),
-                      secondaryBackground: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [Icon(Icons.delete_outline_rounded)],
-                      ),
-                      direction: DismissDirection.endToStart,
-                      onDismissed: (_) {
-                        var removed = tasbihController.tasbihs[index];
-                        tasbihController.tasbihs.removeAt(index);
-                        Get.snackbar(
-                          '${removed.name} dihapus',
-                          'Tekan Urungkan untuk mengembalikan.',
-                          mainButton: TextButton(
-                            child: Text(
-                              'Urungkan',
-                              style: TextStyle(color: Colors.black),
-                            ),
-                            onPressed: () {
-                              if (removed == null) {
-                                return;
-                              }
-                              tasbihController.tasbihs.insert(index, removed);
-                              removed = null;
-                              if (Get.isSnackbarOpen!) {
-                                Get.back();
-                              }
-                            },
-                          ),
-                        );
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.all(Radius.circular(20)),
-                        ),
-                        child: ListTile(
-                          title: Text(tasbihController.tasbihs[index].name),
-                          subtitle: Text(
-                            'Digunakan pada $formattedUpdatedAt',
-                          ),
-                          trailing: Container(
-                            height: 35,
-                            width: 35, // It shows max of 3 digits number
-                            padding: EdgeInsets.all(2),
-                            alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                              color: Theme.of(context).primaryColor,
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(20)),
-                            ),
-                            child: Text(
-                              '${tasbihController.tasbihs[index].count}',
-                              style: TextStyle(
-                                color: Colors.white,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ),
-                          ),
-                          onTap: () {
-                            Get.toNamed('/counter?index=' + index.toString());
-                          },
-                          onLongPress: () {
-                            Fluttertoast.showToast(
-                              msg: "Geser ke kiri untuk menghapus",
-                              toastLength: Toast.LENGTH_LONG,
-                              webPosition: 'center',
-                              webBgColor: '#000',
-                              timeInSecForIosWeb: 3,
+                      backgroundColor: Colors.transparent,
+                      performsFirstActionWithFullSwipe: false,
+                      deleteAnimationDuration: 400,
+                      trailingActions: <SwipeAction>[
+                        SwipeAction(
+                          icon: Icon(Icons.edit),
+                          color: Colors.transparent,
+                          onTap: (CompletionHandler handler) async {
+                            /// Show dialog
+                            _showTextEditingDialog(
+                              context: context,
+                              indexToEdit: index, // Should not be a String
                             );
+
+                            /// Move the list tile back to original position
+                            handler(false);
                           },
                         ),
-                      ),
+                        SwipeAction(
+                          icon: Icon(Icons.delete),
+                          color: Colors.transparent,
+                          onTap: (CompletionHandler handler) async {
+                            /// Vibrate
+                            Feedback.forLongPress(context);
+
+                            /// Remove item from ListView
+                            await handler(true);
+
+                            /// Remove data from database
+                            _removeItem(index);
+                          },
+                        ),
+                      ],
+                      child:
+                          // ListTile
+                          _buildItem(tasbihController.tasbihs[index], index),
                     );
                   },
                 ),
@@ -192,6 +121,158 @@ class HomeScreen extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildItem(var item, int index) {
+    /// Formatting the date
+    DateTime originalUpdatedAt =
+        DateTime.parse(tasbihController.tasbihs[index].updatedAt);
+    final DateFormat formatter = DateFormat('dd-MM-yyyy');
+    final String formattedUpdatedAt = formatter.format(originalUpdatedAt);
+
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+      ),
+      child: ListTile(
+        title: Text(tasbihController.tasbihs[index].name),
+        subtitle: Text(
+          'Digunakan pada $formattedUpdatedAt',
+        ),
+        trailing: Container(
+          height: 35,
+          width: 35, // It shows max of 3 digits number
+          padding: EdgeInsets.all(2),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.green,
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+          ),
+          child: Text(
+            '${tasbihController.tasbihs[index].count}',
+            style: TextStyle(
+              color: Colors.white,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        ),
+        onTap: () {
+          Get.toNamed('/counter?index=' + index.toString());
+        },
+        onLongPress: () {
+          Fluttertoast.showToast(
+            msg: "Geser ke kiri untuk menghapus",
+            toastLength: Toast.LENGTH_LONG,
+            webPosition: 'center',
+            webBgColor: '#000',
+            timeInSecForIosWeb: 3,
+          );
+        },
+      ),
+    );
+  }
+
+  void _removeItem(int index) {
+    var removed = tasbihController.tasbihs[index];
+
+    /// Remove data from database
+    tasbihController.remove(index);
+
+    Get.snackbar(
+      '${removed.name} dihapus',
+      'Tekan Urungkan untuk mengembalikan.',
+      backgroundColor: Colors.white,
+      boxShadows: [
+        BoxShadow(
+          offset: Offset(0, 3),
+          blurRadius: 20,
+          color: Colors.black26,
+        ),
+      ],
+      mainButton: TextButton(
+        child: Text(
+          'Urungkan',
+          style: TextStyle(color: Colors.black),
+        ),
+        onPressed: () {
+          if (removed == null) {
+            return;
+          }
+
+          /// Reinsert the removed data to database
+          tasbihController.tasbihs.insert(index, removed);
+          print('[DATABASE] undo the delete');
+
+          removed = null;
+          if (Get.isSnackbarOpen!) {
+            Get.back();
+          }
+        },
+      ),
+    );
+  }
+
+  void _showTextEditingDialog({
+    required BuildContext context,
+    int? indexToEdit,
+  }) {
+    final bool isEditMode = indexToEdit != null ? true : false;
+
+    /// Fill text field with current tasbih name if [isEditMode] is [true]
+    textEditingController.text =
+        isEditMode ? tasbihController.tasbihs[indexToEdit].name : '';
+
+    final onSubmit = () {
+      /// Tell user to fill the text field if it is is empty
+      if (textEditingController.text.isEmpty) {
+        textFieldFocusNode.requestFocus();
+        Fluttertoast.showToast(
+          msg: 'Nama tidak boleh kosong',
+          toastLength: Toast.LENGTH_LONG,
+          webPosition: 'center',
+          webBgColor: '#000',
+          timeInSecForIosWeb: 3,
+        );
+      } else {
+        if (isEditMode) {
+          /// Update the selected data in database
+          tasbihController.rename(indexToEdit, textEditingController.text);
+        } else {
+          /// Insert data to database
+          tasbihController.insert(textEditingController.text);
+        }
+        Get.back();
+        textEditingController.clear();
+      }
+    };
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(isEditMode ? 'Ubah nama tasbih' : 'Tambah tasbih'),
+          content: TextField(
+            controller: textEditingController,
+            autofocus: true,
+            focusNode: textFieldFocusNode,
+            onSubmitted: (value) => onSubmit,
+            decoration: InputDecoration(
+              labelText: 'Tulis nama tasbih...',
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text(
+                'Simpan',
+              ),
+              onPressed: onSubmit,
+            ),
+          ],
+          elevation: 24,
+        );
+      },
     );
   }
 }
