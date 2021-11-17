50747031b904d3231113dffbb27675bc5b32dbc6
Hary Suryanto <hary.suryanto01@gmail.com>
2021-11-17 17:18:38 +0700

    add responsive design

--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -8,48 +8,54 @@ class CounterScreen extends StatelessWidget {
   final tasbihController = Get.find<TasbihController>();
 
   final index = int.parse(Get.parameters['index']!);
+  final double resposiveWidth = Get.width > 768 ? 768 : Get.width;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            /// Toolbar
-            Container(
-              padding: const EdgeInsets.only(top: 30, left: 30, right: 30),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  IconButton(
-                    padding: EdgeInsets.zero,
-                    icon: Icon(Icons.arrow_back),
-                    onPressed: () {
-                      Get.back();
-                    },
+        child: Center(
+          child: Container(
+            width: resposiveWidth,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                /// Toolbar
+                Container(
+                  padding: const EdgeInsets.only(top: 30, left: 30, right: 30),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      IconButton(
+                        padding: EdgeInsets.zero,
+                        icon: Icon(Icons.arrow_back),
+                        onPressed: () {
+                          Get.back();
+                        },
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'Kamu memilih',
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        tasbihController.tasbihs[index].name,
+                        style: TextStyle(fontSize: 30),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ],
                   ),
-                  SizedBox(height: 8),
-                  Text(
-                    'Kamu memilih',
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    tasbihController.tasbihs[index].name,
-                    style: TextStyle(fontSize: 30),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ],
-              ),
-            ),
+                ),
 
-            SizedBox(height: 20),
+                SizedBox(height: 20),
 
-            /// Counter section
-            Expanded(
-              child: Counter(index: index),
+                /// Counter section
+                Expanded(
+                  child: Counter(index: index),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,63 +10,71 @@ import '../widgets/text_editing_dialog.dart';
 class HomeScreen extends StatelessWidget {
   final themeController = Get.find<ThemeController>();
 
+  final double resposiveWidth = Get.width > 768 ? 768 : Get.width;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: Drawer(child: DrawerContent()),
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            /// Toolbar
-            Container(
-              padding: const EdgeInsets.only(top: 30, left: 30, right: 30),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
+        child: Center(
+          child: Container(
+            width: resposiveWidth,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                /// Toolbar
+                Container(
+                  padding: const EdgeInsets.only(top: 30, left: 30, right: 30),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Builder(builder: (context) {
-                        return IconButton(
-                          icon: Icon(Icons.menu),
-                          padding: EdgeInsets.zero,
-                          tooltip: 'Tampilkan opsi lanjut',
-                          onPressed: () {
-                            // Open drawer
-                            Scaffold.of(context).openDrawer();
-                          },
-                        );
-                      }),
-                      Text('Pilih tasbihmu'),
+                      Row(
+                        children: [
+                          Builder(builder: (context) {
+                            return IconButton(
+                              icon: Icon(Icons.menu),
+                              padding: EdgeInsets.zero,
+                              tooltip: 'Tampilkan opsi lanjut',
+                              onPressed: () {
+                                // Open drawer
+                                Scaffold.of(context).openDrawer();
+                              },
+                            );
+                          }),
+                          Text('Pilih tasbihmu'),
+                        ],
+                      ),
+                      IconButton(
+                        icon: Icon(
+                          Icons.add,
+                        ),
+                        padding: EdgeInsets.zero,
+                        tooltip: 'Buat tasbih baru',
+                        onPressed: () {
+                          // Show dialog
+                          TextEditingDialog.showTextEditingDialog(
+                              context: context);
+                        },
+                      ),
                     ],
                   ),
-                  IconButton(
-                    icon: Icon(
-                      Icons.add,
-                    ),
-                    padding: EdgeInsets.zero,
-                    tooltip: 'Buat tasbih baru',
-                    onPressed: () {
-                      // Show dialog
-                      TextEditingDialog.showTextEditingDialog(context: context);
-                    },
-                  ),
-                ],
-              ),
+                ),
+
+                SizedBox(height: 20),
+
+                /// List of tasbih
+                Expanded(
+                  child: TasbihList(),
+                ),
+
+                /// Guide
+                GetBuilder<ThemeController>(builder: (context) {
+                  return Guide();
+                }),
+              ],
             ),
-
-            SizedBox(height: 20),
-
-            /// List of tasbih
-            Expanded(
-              child: TasbihList(),
-            ),
-
-            /// Guide
-            GetBuilder<ThemeController>(builder: (context) {
-              return Guide();
-            }),
-          ],
+          ),
         ),
       ),
     );
