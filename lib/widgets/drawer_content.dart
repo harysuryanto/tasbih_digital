971ebb4c63073ad960e10a76850232644f0162f0
Hary Suryanto <hary.suryanto01@gmail.com>
2022-01-23 13:09:08 +0700

    give condition where the vibration toggles are only visible on mobile platform

--- lib/widgets/drawer_content.dart
+++ lib/widgets/drawer_content.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_instance/src/extension_instance.dart';
-import 'package:get/state_manager.dart';
+import 'package:get/get.dart';
 
 import '../controllers/tasbih_settings_controller.dart';
 import 'change_theme_button.dart';
@@ -24,36 +23,38 @@ class DrawerContent extends StatelessWidget {
           trailing: ChangeThemeButton(),
         ),
 
-        /// Long vibration each 33
-        ListTile(
-          key: UniqueKey(),
-          leading: Icon(Icons.vibration_outlined),
-          title: Text('Getar panjang setiap 33'),
-          trailing: GetBuilder<TasbihSettingsController>(
-            builder: (context) {
+        /// Only show on mobile platform
+        if (GetPlatform.isMobile) ...[
+          /// Long vibration at multiples of 33
+          ListTile(
+            leading: Icon(Icons.vibration_outlined),
+            title: Text('Getar panjang setiap 33'),
+            trailing: GetBuilder<TasbihSettingsController>(
+              builder: (context) {
+                return Switch.adaptive(
+                  value: tasbihSettingsController.longVibrateEach33,
+                  onChanged: (_) {
+                    tasbihSettingsController.toggleLongVibrateEach33();
+                  },
+                );
+              },
+            ),
+          ),
+
+          /// Long vibration at 100
+          ListTile(
+            leading: Icon(Icons.vibration_outlined),
+            title: Text('Getar panjang pada 100'),
+            trailing: GetBuilder<TasbihSettingsController>(builder: (context) {
               return Switch.adaptive(
-                value: tasbihSettingsController.longVibrateEach33,
-                onChanged: (_) {
-                  tasbihSettingsController.toggleLongVibrateEach33();
+                value: tasbihSettingsController.longVibrateAt100,
+                onChanged: (value) {
+                  tasbihSettingsController.toggleLongVibrateAt100();
                 },
               );
-            },
+            }),
           ),
-        ),
-
-        /// Long vibration at 100
-        ListTile(
-          leading: Icon(Icons.vibration_outlined),
-          title: Text('Getar panjang pada 100'),
-          trailing: GetBuilder<TasbihSettingsController>(builder: (context) {
-            return Switch.adaptive(
-              value: tasbihSettingsController.longVibrateAt100,
-              onChanged: (value) {
-                tasbihSettingsController.toggleLongVibrateAt100();
-              },
-            );
-          }),
-        ),
+        ],
       ],
     );
   }
