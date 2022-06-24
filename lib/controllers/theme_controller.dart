9da44a4324147376cef9230f4d555d7e1e0999e7
Hary Suryanto <hary.suryanto01@gmail.com>
2022-06-24 20:43:12 +0700

    fix theming issue caused by GetX version upgrade

--- lib/controllers/theme_controller.dart
+++ lib/controllers/theme_controller.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/extension_navigation.dart';
-import 'package:get/state_manager.dart';
+import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
 class ThemeController extends GetxController {
--- lib/widgets/change_theme_button.dart
+++ lib/widgets/change_theme_button.dart
@@ -1,22 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_instance/src/extension_instance.dart';
-import 'package:get/get_navigation/src/extension_navigation.dart';
-import 'package:get/get_state_manager/src/simple/get_state.dart';
+import 'package:get/get.dart';
 
 import '../controllers/theme_controller.dart';
+import 'restart_widget.dart';
 
-class ChangeThemeButton extends StatelessWidget {
+class ChangeThemeButton extends StatefulWidget {
+  const ChangeThemeButton({Key? key}) : super(key: key);
+
+  @override
+  State<ChangeThemeButton> createState() => _ChangeThemeButtonState();
+}
+
+class _ChangeThemeButtonState extends State<ChangeThemeButton> {
+  bool isProcessing = false;
+
   @override
   Widget build(BuildContext context) {
     final themeController = Get.find<ThemeController>();
 
     return GetBuilder<ThemeController>(
-      builder: (context) {
+      builder: (_) {
+        if (isProcessing) return CircularProgressIndicator.adaptive();
+
         return Switch.adaptive(
           value: Get.isDarkMode,
           onChanged: (value) {
+            setState(() => isProcessing = !isProcessing);
+
             themeController.toggleTheme();
+
+            Future.delayed(Duration(milliseconds: 1000)).then((value) {
+              RestartWidget.restartApp(context);
+              setState(() => isProcessing = !isProcessing);
+            });
           },
         );
       },
