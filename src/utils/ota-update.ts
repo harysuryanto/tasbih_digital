0124657b3b9775ccbf3e18181411641900b09509
Hary Suryanto <hary.suryanto01@gmail.com>
2023-11-28 23:46:13 +0700

    Show Toast and alert to tell if EAS Updates is working

--- src/utils/ota-update.ts
+++ src/utils/ota-update.ts
@@ -1,11 +1,14 @@
 import * as Updates from "expo-updates";
 import { Alert } from "react-native";
+import Toast from "react-native-simple-toast";
 
 export async function checkOtaUpdate() {
   try {
+    Toast.show("Memeriksa pembaharuan...", Toast.SHORT);
     const update = await Updates.checkForUpdateAsync();
 
     if (update.isAvailable) {
+      Toast.show("Mengunduh versi terbaru...", Toast.LONG);
       await Updates.fetchUpdateAsync();
 
       Alert.alert(
@@ -17,5 +20,7 @@ export async function checkOtaUpdate() {
         ]
       );
     }
-  } catch (error) {}
+  } catch (error) {
+    alert(error);
+  }
 }
