7b56c87e1a09bd702d291592f812fcb12865327b
Hary Suryanto <hary.suryanto01@gmail.com>
2024-10-20 22:50:06 +0700

    Fix redirection issue

--- src/hooks/useAuthRedirection.tsx
+++ src/hooks/useAuthRedirection.tsx
@@ -9,11 +9,19 @@ export default function useAuthRedirection() {
   const { isLoading: isLoadingAuth, authState } = useAuth();
 
   useEffect(() => {
+    const isInSplashScreen = segments[0] === "splash-screen";
     const isInPrivateArea =
       segments[0] === "(tabs)" || segments[0] === "counter";
     const isSignedIn = !!authState;
 
-    if (isLoadingAuth) return;
+    if (isLoadingAuth) {
+      return;
+    }
+
+    if (isInSplashScreen) {
+      router.replace(isSignedIn ? "/home" : "/sign-in");
+      return;
+    }
 
     if (isSignedIn && !isInPrivateArea) {
       router.replace("/home");
