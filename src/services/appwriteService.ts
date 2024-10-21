fb79bf568a0a1c42d87bd6c88a61fc15ad0a9525
Hary Suryanto <hary.suryanto01@gmail.com>
2024-10-22 01:24:34 +0700

    Add .setEndpoint

--- src/services/appwriteService.ts
+++ src/services/appwriteService.ts
@@ -2,5 +2,6 @@ import { Account, Client } from "react-native-appwrite";
 
 export const client = new Client()
   .setProject("6710b5a600394e11084c")
+  .setEndpoint("https://cloud.appwrite.io/v1")
   .setPlatform("com.harysuryanto.tasbihdigital");
 export const account = new Account(client);
