667d0598259747cce5334b4aef4a026b9d91c6f7
Hary Suryanto <hary.suryanto01@gmail.com>
2024-10-27 00:48:35 +0700

    Finish Sign In & Sign Up UI

--- app/(public)/sign-in/index.tsx
+++ app/(public)/sign-in/index.tsx
@@ -1,37 +1,80 @@
 import { View, StyleSheet } from "react-native";
-import { Button, TextInput } from "react-native-paper";
+import { Button, SegmentedButtons, TextInput } from "react-native-paper";
 import React, { useState } from "react";
 import { useAuth } from "@/src/contexts/AuthContext";
 
 export default function SignInScreen() {
-  const { signIn, signInWithGoogle, signUp, signOut } = useAuth();
+  const { signIn, signUp } = useAuth();
 
+  const [activeTab, setActiveTab] = useState("signin");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
 
   return (
     <View style={styles.container}>
-      <TextInput
-        placeholder="Email"
-        value={email}
-        onChangeText={(text) => setEmail(text)}
+      <SegmentedButtons
+        value={activeTab}
+        onValueChange={setActiveTab}
+        buttons={[
+          {
+            value: "signin",
+            label: "Sign In",
+          },
+          {
+            value: "signup",
+            label: "Sign Up",
+          },
+        ]}
       />
-      <TextInput
-        placeholder="Password"
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-        secureTextEntry
-      />
-      <TextInput
-        placeholder="Name"
-        value={name}
-        onChangeText={(text) => setName(text)}
-      />
-      <Button onPress={signInWithGoogle}>Login with Google</Button>
-      <Button onPress={() => signIn(email, password)}>Login</Button>
-      <Button onPress={() => signUp(email, password, name)}>Register</Button>
-      <Button onPress={signOut}>Logout</Button>
+      {activeTab === "signin" ? (
+        <>
+          <TextInput
+            label="Email"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+            mode="outlined"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          <TextInput
+            label="Password"
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            mode="outlined"
+            secureTextEntry
+          />
+          <Button onPress={() => signIn(email, password)}>Sign In</Button>
+          {/* <Button onPress={signInWithGoogle}>Sign In with Google</Button> */}
+        </>
+      ) : (
+        <>
+          <TextInput
+            label="Email"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+            mode="outlined"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          <TextInput
+            label="Password"
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            mode="outlined"
+            secureTextEntry
+          />
+          <TextInput
+            label="Name"
+            value={name}
+            onChangeText={(text) => setName(text)}
+            mode="outlined"
+            keyboardType="default"
+            autoCapitalize="words"
+          />
+          <Button onPress={() => signUp(email, password, name)}>Sign Up</Button>
+        </>
+      )}
     </View>
   );
 }
@@ -41,6 +84,7 @@ const styles = StyleSheet.create({
     alignItems: "stretch",
     backgroundColor: "white",
     flex: 1,
+    gap: 8,
     justifyContent: "center",
     padding: 20,
   },
