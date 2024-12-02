5da5653ee320e61e26e82df2722f961eeb095550
Hary Suryanto <hary.suryanto01@gmail.com>
2024-12-03 00:25:29 +0700

    Update UI

--- app/splash-screen/index.tsx
+++ app/splash-screen/index.tsx
@@ -1,17 +1,23 @@
 import { Text } from "react-native";
 import React from "react";
+import ScreenWrapper from "@/src/components/ScreenWrapper";
+
+const appVersion: string = require("@/package.json").version;
 
 export default function SplashScreen() {
   return (
-    <Text
-      style={{
-        backgroundColor: "white",
-        flex: 1,
-        textAlign: "center",
-        textAlignVertical: "center",
-      }}
-    >
-      Splash Screen
-    </Text>
+    <ScreenWrapper withScrollView={false} style={{ justifyContent: "center" }}>
+      <Text
+        style={{
+          fontFamily: "SpaceMono",
+          fontSize: 32,
+          lineHeight: 32 * 1.5,
+          textAlign: "center",
+        }}
+      >
+        Tasbih Digital
+      </Text>
+      <Text style={{ fontSize: 12, textAlign: "center" }}>v{appVersion}</Text>
+    </ScreenWrapper>
   );
 }
