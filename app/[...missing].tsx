a39af09e2f9f598acac50fb4d11fcf470c873448
Hary Suryanto <hary.suryanto01@gmail.com>
2024-12-27 02:39:16 +0700

    Import React in NotFoundScreen to fix warning

--- app/[...missing].tsx
+++ app/[...missing].tsx
@@ -1,5 +1,6 @@
-import { Link, Stack } from "expo-router";
+import React from "react";
 import { StyleSheet, View, Text } from "react-native";
+import { Link, Stack } from "expo-router";
 
 export default function NotFoundScreen() {
   return (
